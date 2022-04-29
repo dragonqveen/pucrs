41c110fd4e01ad830bb528092d02c8224dc9ad9d
Daniele Paz Machado Gomes <Daniele.Paz@edu.pucrs.br>
2022-04-29 11:11:05 -0300

    :sparkles: Add file manipulation

--- file_manipulation/app.py
+++ file_manipulation/app.py
@@ -1,32 +1,47 @@
+import sys
+
 # generates 1.txt file with number column ranging (1, 20)]
-one = open("1.txt", "w")
-one.write('\n'.join(str(i) for i in range(1, 20 + 1)))
-one.close()
+with open("1.txt", "w") as one:
+    one.write('\n'.join(str(i) for i in range(1, 20 + 1)))
 
 # generates 2.txt file with number column ranging (-20, -1)
-two = open("2.txt", "w")
-two.write('\n'.join(str(i) for i in range(-20, -1 + 1)))
-two.close()
+with open("2.txt", "w") as two:
+    two.write('\n'.join(str(i) for i in range(-20, -1 + 1)))
 
 # merge 1.txt and 2.txt into 1+2.txt
-one = open("1.txt", "r")
-two = open("2.txt", "r")
+with open("1.txt", "r") as one:
+    one_content = one.read()
+    one_content = one_content.split()
 
-one_two = open("1+2.txt", "w")
-one_two.write(two.read() + "\n" + one.read())
+with open("2.txt", "r") as two: 
+    two_content = two.read()
+    two_content = two_content.split()
 
-one_two.close()
-one.close()
-two.close()
+with open("1+2.txt", "w") as one_two:
+    merge = [str(two_content[i] + " " + one_content[i]) for i in range(20)]
 
-#########################################################################
-
-
-
-
-#########################################################################
-
-
+    one_two.write('\n'.join(merge))
 
 
 #########################################################################
+
+# gets size in bytes of the content until value
+def bytes_until(value):
+    cont = 0
+    value_occ = 0
+
+    for i in merge:
+        cont += 1
+
+        if(i == value):
+            print("Tamanho ateh o valor: " + str(sys.getsizeof(merge[0:cont])))
+            value_occ = cont
+        else:
+            print(i)
+    
+    print("Tamanho depois do valor: " + str(sys.getsizeof(merge[value_occ:-1])))
+
+bytes_until("-15 6")
+
+#########################################################################
+
